accc40dff87041427dc75d91cecbe7be70cc6b13
Алексей Иванов <Alessio18911@gmail.com>
2019-10-02 11:33:43 +0300

    Рефакторинг имён переменных и констант, 1 часть

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,31 @@
 'use strict';
 
+var FONT = '16px PT Mono';
+var TEXT_COLOR = '#000000';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_PADDING_LEFT = 40;
+var CLOUD_PADDING_BOTTOM = CLOUD_Y + CLOUD_HEIGHT - 37;
 var CLOUD_COLOR = '#ffffff';
 
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-var SHADOW_X = CLOUD_X + 10;
-var SHADOW_Y = CLOUD_Y + 10;
+var SHADOW_OFFSET_X = 10;
+var SHADOW_OFFSET_Y = 10;
+var SHADOW_X = CLOUD_X + SHADOW_OFFSET_X;
+var SHADOW_Y = CLOUD_Y + SHADOW_OFFSET_Y;
 
-var FONT = '16px PT Mono';
-var TEXT_COLOR = '#000000';
-
-var PADDING_LEFT = CLOUD_X + 40;
-var COLUMN_PADDING_BOTTOM = CLOUD_Y + CLOUD_HEIGHT - 37;
 var HIST_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
-var COLUMN_OFFSET = 50;
-var NEXT_COLUMN_X = COLUMN_WIDTH + COLUMN_OFFSET;
+var COLUMN_OFFSET_X = 50;
+var NEXT_COLUMN_X = COLUMN_WIDTH + COLUMN_OFFSET_X;
 var MY_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
 
-var renderRectangle = function (ctx, color, offsetX, offsetY, width, height) {
+var renderRectangle = function (ctx, color, rectX, rectY, width, height) {
   ctx.fillStyle = color;
-  ctx.fillRect(offsetX, offsetY, width, height);
+  ctx.fillRect(rectX, rectY, width, height);
 };
 
 var getDivisionValue = function (times) {
@@ -53,24 +55,24 @@ var setTextParams = function (ctx) {
   ctx.fillStyle = TEXT_COLOR;
 };
 
-var outputName = function (ctx, offsetX, name) {
+var outputName = function (ctx, name, nameX) {
   setTextParams(ctx);
-  var offsetY = COLUMN_PADDING_BOTTOM + 20;
-  ctx.fillText(name, offsetX, offsetY);
+  var nameY = CLOUD_PADDING_BOTTOM + 20;
+  ctx.fillText(name, nameX, nameY);
 };
 
-var outputTime = function (ctx, offsetX, time, columnHeight) {
+var outputTime = function (ctx, timeX, time, columnHeight) {
   setTextParams(ctx);
-  var offsetY = COLUMN_PADDING_BOTTOM - columnHeight - 10;
-  ctx.fillText(Math.round(time), offsetX, offsetY);
+  var timeY = CLOUD_PADDING_BOTTOM - columnHeight - 10;
+  ctx.fillText(Math.round(time), timeX, timeY);
 };
 
 var outputTitle = function (ctx) {
   setTextParams(ctx);
-  var offsetX = CLOUD_X + 20;
-  var offsetY = CLOUD_Y + 30;
-  ctx.fillText('Ура, Вы победили!', offsetX, offsetY);
-  ctx.fillText('Список результатов:', offsetX, offsetY + 20);
+  var titleX = CLOUD_X + 20;
+  var titleY = CLOUD_Y + 30;
+  ctx.fillText('Ура, Вы победили!', titleX, titleY);
+  ctx.fillText('Список результатов:', titleX, titleY + 20);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -78,16 +80,16 @@ window.renderStatistics = function (ctx, names, times) {
   renderRectangle(ctx, CLOUD_COLOR, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
   outputTitle(ctx);
 
-  var startOffsetX = PADDING_LEFT;
+  var startColumnX = CLOUD_X + CLOUD_PADDING_LEFT;
 
   for (var i = 0; i < times.length; i++) {
     var columnHeight = getColumnHeight(times, times[i]);
     var columnColor = getColumnColor(names[i]);
 
-    outputName(ctx, startOffsetX, names[i]);
-    outputTime(ctx, startOffsetX, times[i], columnHeight);
-    renderRectangle(ctx, columnColor, startOffsetX, COLUMN_PADDING_BOTTOM, COLUMN_WIDTH, -columnHeight);
+    outputName(ctx, names[i], startColumnX);
+    outputTime(ctx, startColumnX, times[i], columnHeight);
+    renderRectangle(ctx, columnColor, startColumnX, CLOUD_PADDING_BOTTOM, COLUMN_WIDTH, -columnHeight);
 
-    startOffsetX += NEXT_COLUMN_X;
+    startColumnX += NEXT_COLUMN_X;
   }
 };
